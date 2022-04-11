b24bc0f6abc1876562f6b7ca3e8a581a980de0d0
panyue <90938238g@gmail.com>
2022-04-11 10:05:49 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import  { ContextProvider } from './context/store'
+
+axios.defaults.baseURL = 'http://localhost:5001/api'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ContextProvider>
+        <App />
+        <ToastContainer />
+      </ContextProvider>
+    </BrowserRouter>
+   </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
