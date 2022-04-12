bd6ae71e6dd0b661b19590f388766c3be1486f6a
panyue <90938238g@gmail.com>
2022-04-12 16:34:45 +0800

    使用useQuery返回的isError和error字段来处理请求error的情况

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,20 +1,27 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchReactHeroData = () => axios.get('http://localhost:4000/superheroes');
+const fetchReactHeroData = async () => {
+    //自动抛出error
+   const { data } = await axios.get('http://localhost:4000/superheroes');
+   return data;
+}
 
 //useQuery抓取数据,取代useState+useEffect
 export const RQSuperHeroesPage = () => {
 
-  const { isLoading, data } = useQuery("react-heroes",fetchReactHeroData);
+  const { isLoading, data, isError, error } = useQuery("super-heroes",fetchReactHeroData);
 
   if(isLoading) return <div>Loading...</div>;
+
+    //如果出错
+  if(isError) return <h2>{error.message}</h2>
 
   return (
       <>
         <h2>UseQuery React Heroes Page</h2>
         {
-          data.data.map(hero => (
+          data.map(hero => (
               <div key={hero.id}>{hero.name}</div>
           ))
         }
