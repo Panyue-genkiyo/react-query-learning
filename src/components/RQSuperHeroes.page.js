2f9f26f1bf72e32a090b9b2dee66168d744f04e0
panyue <90938238g@gmail.com>
2022-04-12 16:13:09 +0800

    使用useQuery hook抓取数据

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchReactHeroData = () => axios.get('http://localhost:4000/superheroes');
+
+//useQuery抓取数据,取代useState+useEffect
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+
+  const { isLoading, data } = useQuery("react-heroes",fetchReactHeroData);
+
+  if(isLoading) return <div>Loading...</div>;
+
+  return (
+      <>
+        <h2>UseQuery React Heroes Page</h2>
+        {
+          data.data.map(hero => (
+              <div key={hero.id}>{hero.name}</div>
+          ))
+        }
+      </>
+  )
 }
