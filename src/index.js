5f5ffec36267ddd213c5059f6bd912711b6df6ec
panyue <90938238g@gmail.com>
2022-04-12 16:42:09 +0800

    添加ReactQueryDevtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
           <App />
+          <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
       </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
