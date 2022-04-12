cf78010625b58b8300a3cc4ecd6a3c4a4572b64e
panyue <90938238g@gmail.com>
2022-04-12 17:05:35 +0800

    探究useQuery data的cache time以及isLoading和isFetching的区别

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -10,12 +10,23 @@ const fetchReactHeroData = async () => {
 //useQuery抓取数据,取代useState+useEffect
 export const RQSuperHeroesPage = () => {
 
-  const { isLoading, data, isError, error } = useQuery("super-heroes",fetchReactHeroData);
+  const { isLoading, data, isError, error, isFetching } = useQuery(
+      "super-heroes",
+      fetchReactHeroData,
+      // {
+      //     cacheTime: 5000, //5s 默认每个query的cache时间为5min
+      // }
+  );
 
   if(isLoading) return <div>Loading...</div>;
 
     //如果出错
   if(isError) return <h2>{error.message}</h2>
+
+    //isLoading !== isFetching(初次两者都是true，后面只要在cache time里，isLoading都不会变成true
+    //而isFetching会每次背后请求更新cache数据 所以会存在(isLoading: false, isFetching: true)
+    //每次回到该组件时都会重新背后加载, 更新cache
+    console.log({isLoading, isFetching})
 
   return (
       <>
