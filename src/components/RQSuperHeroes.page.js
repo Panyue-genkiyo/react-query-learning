1bae2f4461e5b18a119bef884cef3943add91d17
panyue <90938238g@gmail.com>
2022-04-12 22:37:26 +0800

    探究useQuery data的refetchOnMount和refetchOnWindowFocus

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -15,11 +15,14 @@ export const RQSuperHeroesPage = () => {
       fetchReactHeroData,
       {
             //cacheTime: 5000, //5s 默认每个query的cache时间为5min
-           /*
+            /*
               在数据不经常变化的情况下，设置在30s内不会背后refetch数据，真正看到cache数据
               超过30s后，在缓冲时间内重新背后refetch最新数据 如此往复
-            */
-            staleTime: 30000
+             */
+            //staleTime: 30000, //默认0s 这里单位毫秒
+            refetchOnMount: true,  //data is fetched on every time the component is mounted 默认行为 (还可以为always，不管状态是否是stale都会再次请求)
+            // refetchOnMount: false, //就第一次请求数据，不会再次请求数据
+           // refetchOnWindowFocus: true //回到application 再次请求
       }
   );
 
