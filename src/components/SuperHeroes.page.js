358657bd3bdd5bcca4d9060dd86963e98e6dc922
panyue <90938238g@gmail.com>
2022-04-12 16:33:43 +0800

    添加error状态，使用useEffect和useState处理错误状态

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -4,11 +4,15 @@ import axios from 'axios'
 //常规方式useState和useEffect管理server state
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:4000/superheroes').then(res => {
       setData(res.data)
+    }).catch(error => {
+      setError(error.message)
+    }).finally(() => {
       setIsLoading(false)
     })
   }, [])
@@ -17,11 +21,13 @@ export const SuperHeroesPage = () => {
     return <h2>Loading...</h2>
   }
 
+  if(error) return <h2>{error}</h2>
+
   return (
     <>
       <h2>Super Heroes Page</h2>
       {data.map(hero => {
-        return <div>{hero.name}</div>
+        return <div key={hero.id}>{hero.name}</div>
       })}
     </>
   )
