dfa1c111acf18adbe0a97d64f887df4d965f5c98
panyue <90938238g@gmail.com>
2022-04-13 15:37:08 +0800

    自定义抓取每个hero数据hook(包含useQuery 注意会将key以queryKey数据的形式传到回调函数中) get data by id

---
+++ src/hooks/useSuperHeroData.js
@@ -0,0 +1,20 @@
+//请求hero详情页数据自定义hook
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroDetail = async ({ queryKey }) => {
+    const id = queryKey[1]; //自动将key传入
+    const { data } = await axios.get(`http://localhost:4000/superheroes/${id}`);
+    return data;
+}
+
+const useSuperHeroData = (id) => {
+    // return useQuery(["superhero", id], () => fetchSuperHeroDetail(id), {
+    //     enabled: !!id, //当id存在是才执行
+    // });
+    return useQuery(["superhero", id], fetchSuperHeroDetail, {
+        enabled: !!id, //当id存在是才执行
+    });
+}
+
+export default useSuperHeroData
